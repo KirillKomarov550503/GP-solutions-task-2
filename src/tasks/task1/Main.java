ab657b6e2a81052a30aa80078526d2d800064035
KirillKomarov550503 <kirya.komarov.97@list.ru>
2018-02-11 14:33:45 +0300

    complete first task

--- src/tasks/task1/Main.java
+++ src/tasks/task1/Main.java
@@ -5,34 +5,42 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class Main {
-    private static String param;
 
-    private static boolean test(String line) {
+    private static boolean test(String line, String param) {
         String[] params = param.split("\\s+");
         String[] words = line.split("\\s+");
-        for(String par : params){
-            Pattern pattern = Pattern.compile(par);
-            Matcher matcher;
-            for (String word : words) {
-                matcher = pattern.matcher(word);
-                if (matcher.matches())
-                    return true;
+        for (String par : params) {
+            try {
+                Pattern pattern = Pattern.compile(par);
+                Matcher matcher;
+                for (String word : words) {
+                    matcher = pattern.matcher(word);
+                    if (matcher.matches())
+                        return true;
+                }
+            } catch (PatternSyntaxException e) {
+                for (String word : words) {
+                    if (param.equals(word))
+                        return true;
+                }
             }
         }
         return false;
     }
 
+
     public static void main(String[] args) {
         System.out.print("Input program parameter: ");
         Scanner scanner = new Scanner(System.in);
-        param = scanner.nextLine();
+        String param = scanner.nextLine();
         List<String> stringList = new ArrayList<>();
         String line;
         System.out.println("Input strings");
         while ((line = scanner.nextLine()).length() != 0) {
-            if (test(line))
+            if (test(line, param))
                 stringList.add(line);
         }
         System.out.print("Program parameter: " + param + "\n" +
